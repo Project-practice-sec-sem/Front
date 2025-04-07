4e34ce2d8ef7afe8cceb00add549f0d842628fc3
DmitryH <dimopsss@yahoo.com>
2025-04-07 20:08:56 +0500

    Add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import Header from "./components/Header.jsx";
+
+
 function App() {
   return (
     <div>
-      <h1>Hello React</h1>
+
+    <Header />
+    <main>
+    </main>
+
+
     </div>
   )
 }
