cccca46d50c2d5b87d11b12e15778aed8520092d
DmitryH <Dimopsss@yahoo.com>
2025-04-08 17:07:43 +0500

    feat: create window AI-Assistant, add styles

---
+++ src/widgets/ Ai-assistant/Ai-assistant.js
@@ -0,0 +1,12 @@
+export const AiStyles = {
+    width: '350px',
+    height: '700px',
+    backgroundColor: '#627799',
+    marginLeft: '50px',
+    marginTop: '70px',
+    borderRadius: '13px',
+    textAlign: 'center',
+    color: 'white',
+    fontSize: '23px',
+    padding: '10px',
+}
---
+++ src/widgets/ Ai-assistant/Ai-assistant.jsx
@@ -0,0 +1,12 @@
+import Box from '@mui/material/Box';
+import { AiStyles } from '../ Ai-assistant/Ai-assistant.js'
+
+
+
+export const AiAssistant = () => {
+    return (
+        <Box component="section" sx={AiStyles}>
+            <p>Уведомления</p>
+        </Box>
+    );
+}
