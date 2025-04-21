6c5eee7530dbea2bbb238554eeea15e3d34732ed
DmitryH <Dimopsss@yahoo.com>
2025-04-21 21:34:07 +0500

    add .idea to gitignore file

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,12 +2,13 @@ import {Header} from "../widgets/Header/Header.jsx";
 import '../../index.css'
 import {ThemeProvider} from "@mui/material";
 import {theme} from "../shared/style/theme/index.ts";
+import { Footer } from '../widgets/Footer/Footer.jsx';
 
 const App  = () => {
   return (
       <ThemeProvider theme={theme}>
           <Header />
-
+          <Footer />
       </ThemeProvider>
   )
 }
---
+++ src/widgets/Footer/Footer.jsx
@@ -0,0 +1,8 @@
+export const Footer = () => {
+  return (
+    <>
+    <div>уроириофро</div>
+    </>
+  )
+
+}
